324564261ba5e4c71fddfcd9ff27a13d75dbc882
Trypezz <89207551+Trypezz@users.noreply.github.com>
2026-09-16 18:57:30 +0200

    Clean-Up Main
    Implemented a default rom for easier runs of the emulator

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,9 @@
-#include "CPU/cpu.hpp"
-#include <iostream>
-
 #define SDL_MAIN_HANDLED
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_main.h>
 
+#include "CPU/cpu.hpp"
 #include "Cartridge/cart.hpp"
 
 int SDLCALL gameboy_emu_runapp_callback(int argc, char *argv[]);
@@ -16,18 +14,17 @@ int main(int argc, char *argv[]) {
 
 int SDLCALL gameboy_emu_runapp_callback(int argc, char *argv[]) {
   if (!SDL_Init(SDL_INIT_VIDEO)) {
+    SDL_Quit();
     return -1;
   }
 
-  if (argc < 2) {
-    printf("Usage: %s <rom.gb>\n", argv[0]);
+  const char *rom_path = (argc >= 2) ? argv[1] : DEFAULT_ROM_PATH;
+  if (!cart_load(rom_path)) {
     SDL_Quit();
     return 1;
   }
 
-  if (cart_load(argv[1])) {
-    cart_print_info();
-  }
+  cart_print_info();
 
   cpu_reset();
   cpu_fetch();
